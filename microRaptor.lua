ee0b7cf360824f51f1f8a612e0f6cf06621ef57a
elihugarret <pmaekb@hotmail.com>
2014-07-03 04:12:07 -0500

    Create microRaptor.lua

---
+++ microRaptor.lua
@@ -0,0 +1,82 @@
+--microRaptor
+
+--Written by Elihu Garret 2013-2014
+--This Lua module is an abstraction of the proteaAudio Lua API by Gerald Franz. 
+--http://viremo.eludi.net/proteaAudio/proteaaudiolua.html
+
+require("proAudioRt")
+--zbstudio -cfg "singleinstance=false; debugger.port = 8173"
+
+
+--Creates an audio device
+if not proAudio.create() then os.exit(1) end
+
+-- A "play function"
+function play(tono,sample, pitch, duration, volumeL, volumeR)
+	local scale = 2^(pitch/tono)
+	local sound = proAudio.soundLoop(sample, volumeL, volumeR, 0, scale)
+  proAudio.sleep(duration)
+  proAudio.soundStop(sound)
+end
+
+function playChord(tono, sample, pitch, duration, volumeL, volumeR, note1, note2)
+   
+  local scale = 2^(pitch/tono)
+  local sound = proAudio.soundLoop(sample,volumeL, volumeR, 0, scale)
+  local sound1 = proAudio.soundLoop(sample,volumeL, volumeR, 0, scale+note1)
+  local sound2 = proAudio.soundLoop(sample,volumeL, volumeR, 0, scale+note2)
+  proAudio.sleep(duration)
+  proAudio.soundStop(sound)
+  proAudio.soundStop(sound1)
+  proAudio.soundStop(sound2)
+  end
+
+-------------------------------------------------------------------------------------
+ 
+ -- Dinosaur "Ugens" 
+ --Dinamic creation of audio samples using raw data
+ 
+ function stegoSaur(freq, duration, sampleRate,f,m,r)
+local data = {}
+	
+  for i = 1,duration*sampleRate do
+		data[i] = math.random((i*math.cos(math.sin(f)))*(freq/math.random(m,r))/sampleRate,-19) 
+	end
+	return proAudio.sampleFromMemory(data, sampleRate)
+end
+
+
+function archaeOpterix(freq, duration, sampleRate,q,w)
+	local data = {}
+	for i = 1,duration*sampleRate do
+		data[i] = math.tan( ((i*(math.pi*math.cosh(q)))*freq/sampleRate)*math.cos(w))
+	end
+	return proAudio.sampleFromMemory(data, sampleRate)
+end
+
+
+function velociRaptor(freq, duration, sampleRate,v)
+	local data = {}
+	for i = 1,duration*sampleRate do
+		data[i] = math.cos( ((i*math.pi)*freq/sampleRate)*math.pi*v)
+	end
+	return proAudio.sampleFromMemory(data, sampleRate)
+  
+end
+
+function pteroSaur (freq, duration , sampleRate)
+  local data = {}
+  for i = 1, duration*sampleRate do
+    data[i] = math.atan2(-4,6)* (i*math.pi)*(freq/(sampleRate)*math.pi)
+  end
+  return proAudio.sampleFromMemory(data, sampleRate)
+end
+
+function mathRaptor (freq, duration, sampleRate,x,y,z)
+  local data = {}
+  for i = 1, duration*sampleRate do
+    data[i] = math.tan(x*(math.cosh(math.pi,y)*(i*math.pi)*(freq/(sampleRate)*z)))
+  end
+  return proAudio.sampleFromMemory(data, sampleRate)
+ end
+
